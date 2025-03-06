c52d5cd072e078f34acd406c871065fc5aa34912
ARKTEEK <minde398@gmail.com>
2025-03-06 17:00:28 +0200

    fix: use tokio async, change error type

--- src/db.rs
+++ src/db.rs
@@ -4,63 +4,71 @@ use once_cell::sync::Lazy;
 use poise::futures_util::{StreamExt, TryStreamExt};
 use serde::{Deserialize, Serialize};
 use std::error::Error;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[native_model(id = 1, version = 1)]
 #[native_db]
 pub struct Birthday {
-    #[primary_key]
-    user_id: u32,
-    date: String,
+  #[primary_key]
+  pub user_id: u64,
+  pub date: String,
 }
 
 impl Birthday {
-    fn new(user_id: u32, date: String) -> Self {
-        Birthday { user_id, date }
-    }
+  fn new(user_id: u64, date: String) -> Self {
+    Birthday { user_id, date }
+  }
 }
 static MODELS: Lazy<Models> = Lazy::new(|| {
-    let mut models = Models::new();
-    models.define::<Birthday>().unwrap();
-    models
+  let mut models = Models::new();
+  models.define::<Birthday>().unwrap();
+  models
 });
 
 pub struct Database<'a> {
-    db: native_db::Database<'a>,
+  db: Arc<Mutex<native_db::Database<'a>>>,
 }
 
 impl<'a> Database<'a> {
-    pub fn new() -> Result<Self, Box<dyn Error>> {
-        let db = Builder::new().create_in_memory(&MODELS)?;
-        Ok(Database { db })
-    }
+  pub fn new() -> Result<Self, db_type::Error> {
+    let db = Builder::new().create_in_memory(&MODELS)?;
+    Ok(Database {
+      db: Arc::new(Mutex::new(db)),
+    })
+  }
 
-    pub fn add_user(&self, user_id: u32, date: String) -> Result<(), Box<dyn Error>> {
-        let birthday = Birthday::new(user_id, date);
-        let rw = self.db.rw_transaction()?;
-        rw.insert(birthday)?;
-        rw.commit()?;
-        Ok(())
-    }
+  pub async fn add_user(&self, user_id: u64, date: String) -> Result<(), db_type::Error> {
+    let mut db = self.db.lock().await;
+    let birthday = Birthday::new(user_id, date);
+    let rw = db.rw_transaction()?;
+    rw.insert(birthday)?;
+    rw.commit()?;
+    Ok(())
+  }
 
-    pub fn remove_user(&self, user_id: u32) -> Result<(), Box<dyn Error>> {
-        let rw = self.db.rw_transaction()?;
-        let birthday: Birthday = rw.get().primary(user_id)?.unwrap();
+  pub async fn remove_user(&self, user_id: u64) -> Result<(), db_type::Error> {
+    let mut db = self.db.lock().await;
+    let rw = db.rw_transaction()?;
+    let birthday: Birthday = rw.get().primary(user_id)?.unwrap();
 
-        rw.remove(birthday)?;
-        rw.commit()?;
-        Ok(())
-    }
+    rw.remove(birthday)?;
+    rw.commit()?;
+    Ok(())
+  }
 
-    pub fn list_users(&self) -> Result<Vec<Birthday>, Box<dyn Error>> {
-        let r = self.db.r_transaction()?;
-        let birthdays: Vec<Birthday> = r.scan().primary()?.all()?.try_collect()?;
-        Ok(birthdays)
-    }
+  pub async fn list_users(&self) -> Result<Vec<Birthday>, db_type::Error> {
+    let db = self.db.lock().await;
+    let r = db.r_transaction()?;
+    let birthdays: Vec<Birthday> = r.scan().primary()?.all()?.try_collect()?;
+    Ok(birthdays)
+  }
 
-    pub fn get_user_birthday(&self, user_id: u32) -> Result<Birthday, Box<dyn Error>> {
-        let r = self.db.r_transaction()?;
-        let birthday: Birthday = r.get().primary(user_id)?.unwrap();
-        Ok(birthday)
-    }
+  pub async fn get_user_birthday(&self, user_id: u64) -> Result<Birthday, db_type::Error> {
+    let db = self.db.lock().await;
+    let r = db.r_transaction()?;
+    let birthday: Birthday = r.get().primary(user_id)?.unwrap();
+    Ok(birthday)
+  }
 }
